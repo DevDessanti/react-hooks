cb0acdc15f70615ae05ca04d3120eececbe61d87
Felipe Dessanti <felipe.campos_3@hotmail.com>
2021-02-22 16:37:02 -0300

    react-js com hooks, tipo: hooks

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import 'fontsource-roboto';
+import {Container, Typography } from "@material-ui/core"
+
+import {validarCPF, validarSenha} from "./models/cadastro"
+import validacoesCadastro from "./contexts/ValidacoesCadastro";
+class App extends Component {
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
+        <validacoesCadastro.Provider value={{cpf: validarCPF, senha: validarSenha, nome:validarSenha}}>
+        <FormularioCadastro aoEnviar={aoEnviarForm}/>
+        </validacoesCadastro.Provider>
+      </Container>
+    );
+  }
+}
+
+function aoEnviarForm(dados){
+  console.log(dados);
+}
+
+export default App;
